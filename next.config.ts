8711a6ca8dfb1a933707921e8e9771caec3ce581
Caua <caua@example.com>
2026-04-22 11:20:45 -0300

    Prototipo03

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
