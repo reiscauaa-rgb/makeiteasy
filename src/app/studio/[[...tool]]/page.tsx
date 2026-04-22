ae40412556524f180d2a9bcf6faa92e277aca6a5
Caua <caua@example.com>
2026-04-22 12:13:46 -0300

    Prototipo05

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -2,16 +2,10 @@
  * This route renders the Sanity Studio embedded at /studio
  * It is intentionally excluded from the root layout (no Header/Footer)
  */
-import dynamic from 'next/dynamic';
-import config from '../../../../sanity.config';
-
-const NextStudioNoSSR = dynamic(
-  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
-  { ssr: false }
-);
+import StudioClient from './StudioClient';
 
 export { metadata, viewport } from 'next-sanity/studio';
 
 export default function StudioPage() {
-    return <NextStudioNoSSR config={config} />;
+    return <StudioClient />;
 }
